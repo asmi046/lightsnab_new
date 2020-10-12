d4caa73264873d91957ad763b39f91c9da14c9f0
pavlik1l <pavlik1l@ya.ru>
2020-10-12 23:45:36 +0300

    index

---
+++ src/js/_custom.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function ($) {
+  function top_btn() {
+    var button = $('.top-btn');
+    var height_page = $(document).outerHeight(true);
+    var delay = 1000;
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > (height_page / 2)) {
+        button.fadeIn();
+      } else {
+        button.fadeOut();
+      }
+    });
+    button.click(function () {
+      $('body, html').animate({
+        scrollTop: 0
+      }, delay);
+    });
+  }
+  top_btn();
+});
